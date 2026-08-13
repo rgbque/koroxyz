11c07b6ee4fa61e74ed3570133201569634ea8df
rgbque <cmdmail126@gmail.com>
2026-08-13 17:34:28 -0400

    Add files via upload

---
+++ popup.js
@@ -0,0 +1,5 @@
+document.getElementById("supportBtn").addEventListener("click", () => {
+  chrome.tabs.create({
+    url: "https://www.pekora.zip/catalog/324687/Donation-to-help-Support-KoroXYZ"
+  });
+});
